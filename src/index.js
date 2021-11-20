b4a39ea8c211bb8292c109279818b0dc4487034f
bala-tringapps <balayogesh.v@tringapps.com>
2021-11-20 13:19:23 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function Bala(){
+  return <h2>Hello, Welcome to the World</h2>
+}
+
+ReactDOM.render(<Bala/>,document.getElementById('root'));
